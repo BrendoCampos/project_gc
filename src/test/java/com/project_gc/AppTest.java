e4f57ac9178541ed0ac77757e40b2a5db98641db
Brendo Campos <bcss@icomp.ufam.edu.br>
2023-10-20 18:01:26 -0400

    Primeiro commit

---
+++ src/test/java/com/project_gc/AppTest.java
@@ -0,0 +1,28 @@
+package com.project_gc;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class AppTest {
+    @Test
+    public void testFahrenheitToCelsius() {
+        App converter = new App();
+        double result = converter.fahrenheitToCelsius(32.0);
+        assertEquals(0.0, result, 0.001);
+    }
+
+    @Test
+    public void testFahrenheitToCelsiusNegative() {
+        App converter = new App();
+        double result = converter.fahrenheitToCelsius(-40.0);
+        assertEquals(-40.0, result, 0.001);
+    }
+
+    @Test
+    public void testFahrenheitToCelsiusPositive() {
+        App converter = new App();
+        double result = converter.fahrenheitToCelsius(212.0);
+        assertEquals(100.0, result, 0.001);
+    }
+}
